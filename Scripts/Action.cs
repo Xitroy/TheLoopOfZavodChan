256b95b35ca5f909b5a180769de9813dfbbc25cb
Ilya <ymh9w@yandex.ru>
2020-10-04 13:02:50 +0300

    initial

---
+++ Scripts/Action.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+    private Player player;
+    private SpriteRenderer eButtonImage;
+    private SpriteRenderer progressbarImage;
+
+    [Range(1, 15)]
+    public float timeToComplete;
+    private float timer;
+
+    public bool actionStarted { get; private set; }
+    public bool actionEnded { get; private set; }
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        eButtonImage = GetComponentInChildren<Animation>().GetComponent<SpriteRenderer>();
+        progressbarImage = GetComponentInChildren<SpriteRenderer>();
+
+        eButtonImage.color = new Color(1, 1, 1, 0);
+        progressbarImage.color = new Color(1, 1, 1, 0);
+    }
+
+    void Update()
+    {
+        if (!actionEnded)
+        {
+            progressbarImage.color = new Color(1, 1, 1, 1 - Vector2.Distance(player.transform.position, transform.position) / 0.5f);
+
+            if (Vector2.Distance(player.transform.position, transform.position) < 0.15)
+            {
+                if (!actionStarted)
+                    eButtonImage.gameObject.GetComponent<Animation>().Play();
+
+                if (Input.GetKeyDown(KeyCode.E))
+                    actionStarted = true;
+
+                if (actionStarted)
+                {
+                    player.movementBlocked = true;
+
+                    timer += Time.deltaTime;
+                    timer = Mathf.Clamp(timer, 0, timeToComplete);
+                    progressbarImage.transform.localScale = new Vector3(Mathf.Lerp(0.025f, 0f, timer / timeToComplete), Mathf.Lerp(0.025f, 0f, timer / timeToComplete));
+                    actionEnded = timer == timeToComplete ? true : false;
+                }
+                else
+                    timer = 0;
+            }
+        }
+        else
+            player.movementBlocked = false;
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Camera cam;
+    private Vector2 direction;
+    private Vector2 mousePosition;
+
+    [Range(0.1f, 1f)]
+    public float speed = 0.5f;
+
+    public bool movementBlocked;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+
+        if (!movementBlocked)
+            rb.MovePosition(rb.position + speed * direction * Time.fixedDeltaTime);
+    }
+}
